2e0afc52c33eda1598907f2b83e1e1d0d734d60a
redst4r <redst4r@web.de>
2023-06-03 13:19:32 -0700

    adding basics

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='cellbrowser_automation',
+      version='0.1',
+      description='Run cellbrowser on multiple datasets, creating a hierachy from a simple .yaml',
+      url='http://github.com/redst4r/cellbrowser_automation/',
+      author='redst4r',
+      maintainer='redst4r',
+      maintainer_email='redst4r@web.de',
+      license='GNU GPL 3',
+      keywords='scrnaseq, scanpy, cellbrowser',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'toolz',
+          'h5py',
+          'numpy',
+          'scipy',
+          'scanpy>1.7',
+          'sctools @git+https://github.com/redst4r/sctools',
+          'yaml', 
+          ],
+      zip_safe=False)
